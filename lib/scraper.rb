e72b045afa63bb31fc38cfe5cc3dc16538244f81
patrickwide <patrickwide254@gmail.com>
2023-06-12 06:23:01 +0300

    Done demo

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# URL of the website to scrape
+url = "https://flatironschool.com/"
+
+# Fetch the HTML content of the web page
+html = URI.open(url)
+
+# Create a Nokogiri document from the HTML content
+doc = Nokogiri::HTML(html)
+
+# Use Nokogiri methods to extract desired data from the document
+# Here's an example of extracting all the links on the page
+links = doc.css('a').map { |link| link['href'] }
+
+# Print the extracted links
+links.each do |link|
+  puts link
+end
